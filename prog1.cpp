62f3a291cd372a2392f84e2bca2b1d2cd56b0e0f
Your Name <your.email@example.com>
2024-08-17 19:10:41 +0530

    neocolab programs

---
+++ prog1.cpp
@@ -0,0 +1,12 @@
+// You are using GCC
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int x=2*n/7;
+    cout<<"Number of attendees on day 1: "<<x<<endl; 
+    cout<<"Number of attendees on day 2: "<<2*x<<endl;
+    cout<<"Number of attendees on day 3: "<<x/2<<endl;
+}
